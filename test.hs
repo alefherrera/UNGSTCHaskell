76c1e43c88c50cbce1b13be5b0b182cecb166171
Alejandro <alef.herrera@gmail.com>
2016-08-17 20:29:34 -0300

    funciones de listas

--- test.hs
+++ test.hs
@@ -2,3 +2,16 @@ duplicar x = x * 2
 promedio x y = (x+y) / 2
 duplicardos x y = duplicar x + duplicar y
 duplicarSiChico x = if x > 100 then x else x * 2
+
+nums = [1,2,3,4,5,6]
+first = head nums
+t = tail nums
+ultimo = last nums
+inicio = init nums
+largo = length nums
+tomar3 = take 3 nums
+tirar3 = drop 3 nums
+maximo = maximum nums
+minimo = minimum nums
+suma = sum nums
+multiplicacion = product nums
