a4a582da9c2e803b571e6fccce72d6288d36670f
Alejandro <alef.herrera@gmail.com>
2016-08-17 20:41:57 -0300

    notacion

--- test.hs
+++ test.hs
@@ -1,8 +1,10 @@
+-- funciones
 duplicar x = x * 2
 promedio x y = (x+y) / 2
 duplicardos x y = duplicar x + duplicar y
 duplicarSiChico x = if x > 100 then x else x * 2
 
+-- listas
 nums = [1,2,3,4,5,6]
 first = head nums
 t = tail nums
@@ -15,3 +17,12 @@ maximo = maximum nums
 minimo = minimum nums
 suma = sum nums
 multiplicacion = product nums
+
+-- rangos
+rango = [1..20]
+tomar10 = take 10 (cycle [1,2,3])
+tomar10repetido = take 10 (repeat 5)
+
+-- declaraciones
+lista1 = [ x * 2 | x <- [1..10]]
+listaCuadrada = [ x ^ 2 | x <- [1..10]]
