19ae95481e971026ef17eef751798e36250f8adf
Alejandro <alef.herrera@gmail.com>
2016-10-23 16:21:01 -0300

    secante

--- tp1.hs
+++ tp1.hs
@@ -7,6 +7,9 @@ newton f p x n = newton f p x (n-1) - f(newton f p x (n-1)) / p(newton f p x (n-
 reduce n a = if n - a < 0 then 0 else n - a
 
 secante f x 0 = x
-secante f x n = secante f x (reduce n 1) - f(secante f x (reduce n 1)) / (f(secante f x (reduce n 2) * f(secante f x (reduce n 1))) / (secante f x (reduce n 2) - secante f x (reduce n 2)))
+secante f x n =
+  secante f x (n-1) - f(secante f x (n-1)) *
+  (secante f x (n-1) - secante f x (n-2)) /
+  (f(secante f x (n-1)) - f(secante f x (n-2)))
 
 rectangulo f a b n = sum[ f((b-a) / n * i) | i <- [1..n] ] * (b - a) / n
