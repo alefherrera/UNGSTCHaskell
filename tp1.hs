199908e647e8deccb494f92f3bd3375e9af9822f
alefherrera <alef.herrera@gmail.com>
2016-10-23 17:33:36 -0300

    funcion de secante y trapecio funcionando correctamente

--- tp1.hs
+++ tp1.hs
@@ -4,12 +4,12 @@ derivada x = 2 * x
 newton f p x 0 = x
 newton f p x n = newton f p x (n-1) - f(newton f p x (n-1)) / p(newton f p x (n-1))
 
-reduce n a = if n - a < 0 then 0 else n - a
-
 secante f x 0 = x
+secante f x 1 = 2 * x
 secante f x n =
   secante f x (n-1) - f(secante f x (n-1)) *
   (secante f x (n-1) - secante f x (n-2)) /
   (f(secante f x (n-1)) - f(secante f x (n-2)))
 
-rectangulo f a b n = sum[ f((b-a) / n * i) | i <- [1..n] ] * (b - a) / n
+rectangulo f a b n = (b - a) / n * sum[ f(a + (b-a) / n * i) | i <- [1..n] ]
+trapecio f a b n = ((b - a) / (2 * n)) * (f(a) +  2 * sum[f(a + (b-a)/n * i) | i <- [2..(n-1)] ] + f(b))
